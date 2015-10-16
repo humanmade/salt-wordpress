b7a5356da7f8fddbc57fc21147077796372ca970
Joe Hoyle <joehoyle@gmail.com>
2015-10-16 11:04:47 -0400

    Treat none as strings

--- config/cloudformation-vars.php
+++ config/cloudformation-vars.php
@@ -1,7 +1,7 @@
 <?php
 
 defined( 'DB_HOST' ) or define( "DB_HOST", "{{ grains['deployment_mysql_hostname'] }}" );
-{% if grains['deployment_mysql_slave_hostname'] == None %}
+{% if grains['deployment_mysql_slave_hostname'] == 'None' %}
 defined( 'DB_HOST_SLAVE' ) or define( "DB_HOST_SLAVE", "" );
 {% else %}
 defined( 'DB_HOST_SLAVE' ) or define( "DB_HOST_SLAVE", "{{ grains['deployment_mysql_slave_hostname'] }}" );
@@ -10,7 +10,7 @@ defined( 'DB_PASSWORD' ) or define( "DB_PASSWORD", "{{ grains['deployment_mysql_
 defined( 'DB_NAME' ) or define( "DB_NAME", "{{ grains['deployment_mysql_db_name'] }}" );
 defined( 'DB_USER' ) or define( "DB_USER", "{{ grains['deployment_mysql_username'] }}" );
 
-{% if grains['deployment_s3_uploads_path'] != None %}
+{% if grains['deployment_s3_uploads_path'] != 'None' %}
 	defined( 'S3_UPLOADS_BUCKET' ) or define( 'S3_UPLOADS_BUCKET', "{{ grains['deployment_s3_uploads_bucket'] }}/{{ grains['deployment_s3_uploads_path'] }}" );
 {% else %}
 	defined( 'S3_UPLOADS_BUCKET' ) or define( 'S3_UPLOADS_BUCKET', "{{ grains['deployment_s3_uploads_bucket'] }}" );
@@ -18,7 +18,7 @@ defined( 'DB_USER' ) or define( "DB_USER", "{{ grains['deployment_mysql_username
 defined( 'S3_UPLOADS_KEY' ) or define( 'S3_UPLOADS_KEY', "{{ grains['deployment_s3_uploads_access_key'] }}" );
 defined( 'S3_UPLOADS_SECRET' ) or define( 'S3_UPLOADS_SECRET', "{{ grains['deployment_s3_uploads_secret_key'] }}" );
 
-{% if grains['deployment_elasticsearch_hostname'] != None %}
+{% if grains['deployment_elasticsearch_hostname'] != 'None' %}
 define( 'ELASTICSEARCH_HOST', '{{ grains['deployment_elasticsearch_hostname'] }}');
 define( 'ELASTICSEARCH_PORT', {{ grains['deployment_elasticsearch_port'] }} );
 {% endif %}
